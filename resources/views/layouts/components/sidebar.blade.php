c527a022d7619509923a84e3a1281a746ee5a6ba
shahed-64 <shahedislam6475@gmail.com>
2025-08-29 00:50:36 +0600

    Inplement Deshboard

---
+++ resources/views/layouts/components/sidebar.blade.php
@@ -0,0 +1,35 @@
+            <div class="sidebar" id="sidebar">
+                <div class="sidebar-inner slimscroll">
+					<div id="sidebar-menu" class="sidebar-menu">
+						<ul>
+							<li class="menu-title"> 
+								<span>Main</span>
+							</li>
+							<li class="active"> 
+								<a href="index.html"><i class="fe fe-home"></i> <span>Dashboard</span></a>
+							</li>
+                            
+							<li class="menu-title"> 
+								<span>UI Interface</span>
+							</li>
+							<li> 
+								<a href="components.html"><i class="fe fe-vector"></i> <span>Borrowings</span></a>
+							</li>
+							<li class="submenu">
+								<a href="#"><i class="fe fe-layout"></i> <span> Students </span> <span class="menu-arrow"></span></a>
+								<ul style="display: none;">
+									<li><a href="form-basic-inputs.html">All Students </a></li>
+									<li><a href="form-input-groups.html">Add New Student </a></li>
+								</ul>
+							</li>
+                            <li class="submenu">
+								<a href="#"><i class="fe fe-layout"></i> <span> Books </span> <span class="menu-arrow"></span></a>
+								<ul style="display: none;">
+									<li><a href="form-basic-inputs.html">All Books </a></li>
+									<li><a href="form-input-groups.html">Add New Book </a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+                </div>
+            </div>
